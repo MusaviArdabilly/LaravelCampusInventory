2b05363ece50a110301258612f23c23c16019642
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-04-03 09:53:03 +0700

    vendor

--- public/vendor/bootstrap/js/bootstrap.js
+++ public/vendor/bootstrap/js/bootstrap.js
@@ -3,6 +3,10 @@
   * Copyright 2011-2019 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
+
+  $('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery'), require('popper.js')) :
   typeof define === 'function' && define.amd ? define(['exports', 'jquery', 'popper.js'], factory) :
