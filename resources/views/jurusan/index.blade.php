4bcc1a2b6c9c07eb4c8057eec15d403342090c79
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-04-03 09:53:48 +0700

    jurusan view

---
+++ resources/views/jurusan/index.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container min-vh-100">
+        <a href="#myModal" data-toggle="modal"><button type="button" class="btn btn-secondary mt-6">Tambah</button></a>
+        <div class="card mt-2 mb-5">
+            <div class="card-header">
+                <div class="h2 d-flex m-0">
+                    Jurusan
+                </div>
+                <div class="d-flex">
+                    <form method="GET" action="{{ url('/jurusan/search') }}">
+                        <div class="input-group mb-3 my-auto">
+                            <input type="text" name="searchInput" class="form-control w-50" placeholder="Type Here...">
+                            <button class="form-control btn-secondary" type="submit">search</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <table class="table mb-0">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">fakultas</th>
+                        <th scope="col">Action</th>
+                        <!-- <th scope="col">Handle</th> -->
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($data as $key => $jurusan)
+                    <tr>
+                        <td scope="row" width="20px">{{ $data->firstItem() + $key }}</td>
+                        <td>{{ $jurusan->name }}</td>
+                        <td>@foreach($fakultas as $f)
+                                        @if($f->id == $jurusan->id_fakultas)
+                                            {{ $f->name }}
+                                        @endif
+                                    @endforeach</td>
+                        <td><a href="{{ url('jurusan/edit/'.$jurusan->id) }}">Edit</a> - <a href="{{ url('jurusan/delete/'.$jurusan->id) }}">Delete</a></td>
+                        <!-- <td>@mdo</td> -->
+                    </tr>
+                    @empty
+                    <tr>
+                      <td colspan="4" class="text-center">Data Tidak Ditemukan</td>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+        {{ $data->links() }}
+    </div>
+
+    <!-- Modal HTML -->
+    <div id="myModal" class="modal fade" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form method="POST" action="{{ url('/jurusan/store') }}" enctype="multipart/form-data">
+                @csrf
+                    <div class="modal-header">
+                        <h5 class="modal-title">Tambah Data</h5>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <label>nama fakultas</label><br>
+                        <select name="id_fakultas" class="form-control">
+                            @foreach($fakultas as $f)
+                            <option value="{{ $f->id }}">{{ $f->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="modal-body">
+                        <input type="text" class="form-control" placeholder="Jurusan" name="name">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                        <button type="sumbit" class="btn btn-primary">Tambah</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+@stop
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,10 +9,15 @@
 	<title>Inventory</title>
 
 	<link href="{{ asset('vendor/bootstrap/css/bootstrap.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+	<script src="{{ asset('https://code.jquery.com/jquery-3.3.1.slim.min.js') }}"></script>
+	<script src="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js') }}"></script>
+	<script src="{{ asset('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js') }}"></script>
 </head>
 <body>
 
-	<nav class="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light">
+	<nav class="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light fixed-top">
 		<div class="container">
 		    <a class="navbar-brand mb-0 h1" href="/">Inventory</a>
 		    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,18 +25,52 @@
 		    </button>
 		    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 			    <div class="navbar-nav ml-auto">
-			      <a class="nav-item nav-link" href="#">Login</a>
-			      <a class="nav-item nav-link" href="#">Register</a>
+			      <a class="nav-item nav-link" href="/login">Login</a>
+			      <a class="nav-item nav-link" href="/register">Register</a>
 			    </div>
 		    </div>
 		</div>
 	</nav>
 
-	<div>
+	<div id="content" class="fixed-left">
+        <div class="wrapper bg-darks">
+	    <!-- Sidebar -->
+	    <nav id="sidebar">
+	        <!-- <div class="sidebar-header">
+	            <h4>Campus Inventory</h4>
+	        </div> -->	        
+	        <div class="h5 mb-2">
+	        	Core
+	        </div>
+	        <div class="h6 text-light ml-2"><a href="#"></a>
+	        	Dashboard
+	        </div>
+	        <div class="h6 ml-2">
+	        	<a href="/fakultas" class="text-light text-decoration-none">Fakultas</a>
+	        </div>
+	        <div class="h6 ml-2 mb-3">
+	        	<a href="/jurusan" class=" text-light text-decoration-none">Jurusan</a>
+	        </div>
+	        <div class="h5 mb-2">
+	        	Additionnal
+	        </div>
+	        <div class="h6 text-light ml-2 mb-3"><a href="#"></a>
+	        	Report
+	        </div>
+	    </nav>
+
+		</div>
+    </div>
+
+	<div class="sidecontent">
 		@yield('content')
 	</div>
 
-	<footer class="bg-dark text-center text-light py-3">Copyright &copy; Musavi Ardabilly Taufik 2020</footer>
+	<footer class="bg-dark text-center text-light py-3 sidecontent">Copyright &copy; Musavi Ardabilly Taufik 2020</footer>
 
+  <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js') }}"></script>
+  <script src="{{ asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js') }}"></script>
 </body>
+
 </html>
