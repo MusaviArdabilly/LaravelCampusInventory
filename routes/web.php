f136a4d81d5b792c7ded65972544075d268b00fe
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-04-07 17:54:02 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -11,38 +11,75 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/', 'PageController@dashboard');
-
-
-Route::get('fakultas', 'FakultasController@index');
- 
-Route::post('fakultas/store', 'FakultasController@store');
-
-Route::get('fakultas/edit/{id}', 'FakultasController@edit');
-
-Route::post('fakultas/update/{id}', 'FakultasController@update');
-
-Route::get('fakultas/delete/{id}', 'FakultasController@destroy'); //
-
- 
-Route::get('jurusan', 'JurusanController@index');
-
-Route::get('jurusan/search', 'JurusanController@search');
- 
-Route::post('jurusan/store', 'JurusanController@store');
-
-Route::get('jurusan/edit/{id}', 'JurusanController@edit');
-
-Route::post('jurusan/update/{id}', 'JurusanController@update');
-
-Route::get('jurusan/delete/{id}', 'JurusanController@destroy');
-
-
-
-Route::get('/login', 'PageController@login');
-
 Route::get('/register', 'PageController@register');
+
+Route::get('/login', 'PageController@login')->name('login');
+
+Route::get('/logout', 'AuthController@logout');
+
+Route::post('/postlogin', 'AuthController@login');
+
+Route::post('/postregister', 'AuthController@register');
+
+
+Route::group(['middleware' => ['auth','role:admin']], function(){
+
+	Route::get('/', 'PageController@dashboard');
+
+
+	Route::get('fakultas', 'FakultasController@index');
+	 
+	Route::post('fakultas/store', 'FakultasController@store');
+
+	Route::get('fakultas/edit/{id}', 'FakultasController@edit');
+
+	Route::post('fakultas/update/{id}', 'FakultasController@update');
+
+	Route::get('fakultas/delete/{id}', 'FakultasController@destroy');
+
+	 
+	Route::get('jurusan', 'JurusanController@index');
+
+	Route::get('jurusan/search', 'JurusanController@search');
+	 
+	Route::post('jurusan/store', 'JurusanController@store');
+
+	Route::get('jurusan/edit/{id}', 'JurusanController@edit');
+
+	Route::post('jurusan/update/{id}', 'JurusanController@update');
+
+	Route::get('jurusan/delete/{id}', 'JurusanController@destroy');
+
+	 
+	Route::get('ruangan', 'RuanganController@index');
+
+	Route::get('ruangan/search', 'RuanganController@search');
+	 
+	Route::post('ruangan/store', 'RuanganController@store');
+
+	Route::get('ruangan/edit/{id}', 'RuanganController@edit');
+
+	Route::post('ruangan/update/{id}', 'RuanganController@update');
+
+	Route::get('ruangan/delete/{id}', 'RuanganController@destroy');
+
+});
+
+ 
+Route::group(['middleware' => ['auth','role:admin,staff']], function(){
+
+	Route::get('/', 'PageController@dashboard');
+
+	Route::get('barang', 'BarangController@index');
+
+	Route::get('barang/search', 'BarangController@search');
+	 
+	Route::post('barang/store', 'BarangController@store');
+
+	Route::get('barang/edit/{id}', 'BarangController@edit');
+
+	Route::post('barang/update/{id}', 'BarangController@update');
+
+	Route::get('barang/delete/{id}', 'BarangController@destroy');
+
+});
