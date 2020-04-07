d27dc325672d406330064db815c735263587bff1
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-04-07 17:54:16 +0700

    view ruangan

---
+++ resources/views/ruangan/edit.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.main')
+
+@section('content')
+<section class="section">
+  
+  <div class="section-header">
+    <h1>
+      Ruangan <small>Edit Data</small>
+    </h1>
+  </div>
+
+  <div class="section-body">
+    <div class="col-12 col-md-6 col-lg-6">
+        <div class="card">
+          <div class="card-header">
+            <a href="{{ url('/ruangan') }}"> 
+              <button type="button" class="btn btn-outline-info">
+                <i class="fas fa-arrow-circle-left"></i> Back
+              </button>
+          </a>
+          </div>
+          <div class="card-body">
+            <form action="{{ url('ruangan/update/'.$ruangan->id) }}" method="POST" enctype="multipart/form-data">
+              @csrf
+              <div class="form-group">
+                <label>Name</label>
+                <input type="text" name="name" class="form-control" value="{{ $ruangan->room }}">
+              </div>
+              <div class="form-group">
+                <button type="submit" class="btn btn-primary">SAVE</button>
+              </div>
+              </form>
+          </div>
+        </div>
+      </div>  
+  </div>
+
+</section>
+@endsection()
---
+++ resources/views/ruangan/index.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container min-vh-100">
+        <a href="#myModal" data-toggle="modal"><button type="button" class="btn btn-secondary mt-6">Tambah</button></a>
+        <div class="card mt-2 mb-5">
+            <div class="card-header">
+                <div class="h2 d-flex m-0">
+                    Ruangan
+                </div>
+                <div class="d-flex">
+                    <form method="GET" action="{{ url('/ruangan') }}">
+                        <div class="input-group mb-3 my-auto">
+                            <input type="text" name="searchInput" class="form-control w-50" placeholder="Type Here...">
+                            <button class="form-control btn-secondary" type="submit">search</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <table class="table mb-0">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Jurusan</th>
+                        <th scope="col">Ruangan</th>
+                        <th scope="col">Action</th>
+                        <!-- <th scope="col">Handle</th> -->
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($data as $key => $ruangan)
+                    <tr>
+                        <td scope="row" width="20px">{{ $data->firstItem() + $key }}</td>
+                        <td>{{ $ruangan->jurusan->major }}</td>
+                        <td>{{ $ruangan->room }}</td>
+                        <td><a href="{{ url('ruangan/edit/'.$ruangan->id) }}">Edit</a> - <a href="{{ url('ruangan/delete/'.$ruangan->id) }}">Delete</a></td>
+                    </tr>
+                    @empty
+                    <tr>
+                      <td colspan="3" class="text-center">Data Tidak Ditemukan</td>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+        {{ $data->links() }}
+    </div>
+
+    <!-- Modal HTML -->
+    <div id="myModal" class="modal fade" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form method="POST" action="{{ url('/ruangan/store') }}" enctype="multipart/form-data">
+                @csrf
+                    <div class="modal-header">
+                        <h5 class="modal-title">Tambah Data</h5>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <label>Nama Jurusan</label><br>
+                        <select name="id_jurusan" class="form-control">
+                            @foreach($jurusan as $data)
+                            <option value="{{ $data->id }}">{{ $data->major }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="modal-body">
+                        <label>Nama Ruangan</label><br>
+                        <input type="text" class="form-control" placeholder="Ruangan" name="room">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                        <button type="sumbit" class="btn btn-primary">Tambah</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+@stop
