d3f00b08510ca6cf1565695d36c64a0c85922201
MusaviArdabillyTaufik <201musavi@gmail.com>
2020-05-06 20:22:56 +0700

    add modal for import excel

--- resources/views/fakultas/index.blade.php
+++ resources/views/fakultas/index.blade.php
@@ -9,6 +9,7 @@
 
     <div class="container min-vh-100">
         <a href="#myModal" data-toggle="modal"><button type="button" class="btn btn-secondary mt-5">Tambah</button></a>
+        <a href="#ModalImport" data-toggle="modal"><button type="button" class="btn btn-outline-secondary mt-5">Import Excel</button></a>
         @if(count($errors) > 0)
         <div class="card-body">
             <div class="alert alert-danger">
@@ -21,7 +22,7 @@
             </div>
         </div>
         @endif
-        <div class="card mt-2 mb-5">
+        <div class="card mt-2 mb-5 table-responsive">
             <div class="card-header">
                 <div class="h2 d-flex m-0">
                     Fakultas
@@ -75,6 +76,29 @@
                     <div class="modal-body">
                         <label>Nama Fakultas</label><br>
                         <input type="text" class="form-control" placeholder="Fakultas" name="faculty">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                        <button type="sumbit" class="btn btn-primary">Tambah</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal Import HTML -->
+    <div id="ModalImport" class="modal fade" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form method="POST" action="{{ url('/fakultas/import') }}" enctype="multipart/form-data">
+                @csrf
+                    <div class="modal-header">
+                        <h5 class="modal-title">Import Excel</h5>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <label>Upload data Fakultas</label><br>
+                        <input type="file" class="form-control" placeholder="Fakultas" name="excel" accept=".xls, .xlsx">
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
